701ba1dd27cb26eb768db730b5c7a60b1e59b816
Peter Okwara <peter.okwara@outlook.com>
2019-11-01 18:56:55 +0300

    Subscribe to the service to fetch products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from "../product";
+import { ProductService } from "../product.service";
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[]
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe(response => this.products = response)
   }
 
 }
