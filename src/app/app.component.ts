9dd54fe4cfca3fdd6fae9289105051af49210fd3
Peter Okwara <peter.okwara@outlook.com>
2019-11-01 17:23:27 +0300

    Remove code, added to wrong file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ProductService } from ''./product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ProductService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
